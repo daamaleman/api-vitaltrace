05a13ded46659757aae293ea1d4c832a62c33807
daamaleman <diedereicha@uamv.edu.ni>
2026-07-23 00:40:44 -0600

    feat(model): add Person model with soft deletes and fillable attributes

---
+++ app/Models/Person.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Person extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'middle_name',
+        'first_last_name',
+        'second_last_name',
+        'date_of_birth',
+        'gender',
+        'identity_document',
+        'phone',
+        'address',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'date_of_birth' => 'date',
+    ];
+}
