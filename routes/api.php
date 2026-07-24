c73328537979a4b9dad8e07291b266b4de162178
daamaleman <diedereicha@uamv.edu.ni>
2026-07-24 00:12:25 -0600

    refactor(routes): namespace all v1 API resource routes under prefix('v1')

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\PersonController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PermissionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,16 +18,20 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function () {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Define API routes for the Role resource
+    Route::apiResource('/roles', RoleController::class);
+
+    // Define API routes for the Person resource
+    Route::apiResource('/people', PersonController::class);
+
+    // Define API routes for the User resource
+    Route::apiResource('/users', UserController::class);
+
+    // Define API routes for the Permission resource
+    Route::apiResource('/permissions', PermissionController::class);
 });
-
-// Define API routes for the Role resource
-Route::apiResource('/roles', RoleController::class);
-
-
-// Define API routes for the Person resource
-Route::apiResource('/people', PersonController::class);
-
-// Define API routes for the User resource
-Route::apiResource('/users', UserController::class);
