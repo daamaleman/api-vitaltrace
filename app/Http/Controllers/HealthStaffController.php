b11b08f5d942ea10604eef2d1c7e2eaefa1606c5
daamaleman <diedereicha@uamv.edu.ni>
2026-07-25 01:53:29 -0600

    feat(api): implement HealthStaff CRUD endpoints with standardized response envelope

---
+++ app/Http/Controllers/HealthStaffController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\HealthStaff;
+use Illuminate\Http\Request;
+
+class HealthStaffController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(HealthStaff $healthStaff)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, HealthStaff $healthStaff)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(HealthStaff $healthStaff)
+    {
+        //
+    }
+}
