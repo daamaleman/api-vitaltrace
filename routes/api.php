b69fd886e23ea49bf517664832fcc47928edd599
daamaleman <diedereicha@uamv.edu.ni>
2026-07-26 00:13:17 -0600

    feat(routes): register Appointment API resource routes under v1 prefix

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ use App\Http\Controllers\TreatmentMedicationController;
 use App\Http\Controllers\MeasurementTypeController;
 use App\Http\Controllers\MeasurementController;
 use App\Http\Controllers\ClinicalRangeController;
+use App\Http\Controllers\AppointmentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -66,4 +67,5 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('measurement-types', MeasurementTypeController::class);
     Route::apiResource('measurements', MeasurementController::class);
     Route::apiResource('clinical-ranges', ClinicalRangeController::class);
+    Route::apiResource('appointments', AppointmentController::class);
 });
